d889477eafb846089fae39cbfdd18984ca47f79e
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-10-30 23:03:53 +0600

    reversed

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// connection mongodb
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.aeutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://tourdaw:dWs74UISToqfpGpC@cluster0.aeutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
