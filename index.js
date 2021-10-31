56bf157a20b8fedde712d2bc214736bcbd511ef3
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-10-31 19:16:04 +0600

    env cng

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://tourdaw:dWs74UISToqfpGpC@cluster0.aeutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aeutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aeutd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
